775fd0ee11111c3e4e984141a2840f870a70a568
James Bradbury <github@jamesbradbury.net>
2022-03-21 15:49:41 +0000

    move fav bundling from javascript -> jsui

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ import { getBabelOutputPlugin }  from '@rollup/plugin-babel';
 export default {
   input: 'src/api-max.js',
   output: {
-    file: 'max-package/javascript/fav-max.js',
+    file: 'max-package/jsui/fav-max.js',
     format: 'cjs',
     exports: 'named'
   },
